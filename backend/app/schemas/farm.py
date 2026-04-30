60a4b7cceee632ca96773c676f8ead110c0dd50e
inamprograms <inamul09rehman@gmail.com>
2026-04-30 17:02:50 +0500

    farm_service: Update farm schemas

--- backend/app/schemas/farm.py
+++ backend/app/schemas/farm.py
@@ -1,6 +1,11 @@
 def farm_schema(data):
     return {
+        "id": data.get("id"),
         "farmer_id": data.get("farmer_id"),
+        "name": data.get("name"),
         "farm_type": data.get("farm_type"),
-        "size_category": data.get("size_category")
+        "size_category": data.get("size_category"),
+        "location": data.get("location"),
+        "description": data.get("description"),
+        "created_at": data.get("created_at"),
     }
